32c7b6ca2db044891caa17ee872fdb7860ed9fe4
Ramon Morales <rayman_mora70@yahoo.com>
2022-04-12 19:56:41 -0500

    Add computerPlay function

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Function for computer to play
 function computerPlay() {
-    
+    // Generate random integer
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(computerChoice);
+
+    if(computerChoice === 1) {
+        let choice = "Rock";
+        return choice;
+    } else if(computerChoice === 2) {
+        let choice = "Paper";
+        return choice;
+    } else {
+        let choice = "Scissor";
+        return choice;
+    }
 }
+console.log(computerPlay)
